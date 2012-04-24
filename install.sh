d6ecafadb3efaa061967951cd6cef44237a511f8
Leandro Facchinetti <leafac@gmail.com>
2012-04-24 18:57:11 -0300

    Remove command-t compile from install script

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo 'Assert that submodules were correctly initialized'
+git submodule init
+git submodule update
+
 echo 'Linking dotfiles on home folder'
 ln -sf $PWD/vim                         ~/.vim
 ln -sf $PWD/vim/vimrc                   ~/.vimrc
@@ -9,35 +13,22 @@ ln -sf $PWD/git/gitmessage              ~/.gitmessage
 ln -sf $PWD/irbrc                       ~/.irbrc
 ln -sf $PWD/tmux.conf                   ~/.tmux.conf
 ln -sf $PWD/oh-my-zsh                   ~/.oh-my-zsh
+ln -sf ~/.oh-my-zsh/zshrc ~/.zshrc
 mkdir -p ~/.fonts
 ln -sf $PWD/fonts/ubuntu-mono-powerline ~/.fonts/ubuntu-mono-powerline
 
 echo 'Install some basic packages'
-sudo apt-get install g++ ncurses-term vim-nox vim-gui-common git ack-grep tig exuberant-ctags tmux zsh
+sudo apt-get install g++ ncurses-term vim-nox vim-gui-common git ack-grep tig exuberant-ctags tmux zsh build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
 
-echo "ack-grep is a lame name! Vi plugin won't recognize it"
+
+echo "ack-grep is a lame name! Vim plugin won't recognize it"
 sudo ln -sf /usr/bin/ack-grep /usr/bin/ack
 
 echo 'Install rvm'
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
-source ~/.bashrc
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
-rvm install 1.8.7
-rvm install 1.9.3
-
-echo 'Assert that submodules were correctly initialized'
-git submodule init
-git submodule update
-
-echo 'Install rake to compile command-t'
-rvm use 1.8.7
-gem install rake
-
-echo 'Compile command-t'
-cd ~/.vim/bundle/command-t
-rake make
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # This loads RVM into a shell session.
+rvm install 1.9.3-head
 rvm use --default 1.9.3
 
-echo 'Configure oh-my-zsh'
-ln -sf ~/.oh-my-zsh/zshrc ~/.zshrc
+echo 'Change default shell to zsh'
 chsh -s /bin/zsh
