c62448ff77d7e3b8a80c07d8e4fcdbe83e8a0def
Leandro Facchinetti <leafac@gmail.com>
2012-05-07 23:49:54 -0300

    Small tweaks on install script

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ ln -sf $PWD/git/gitmessage              ~/.gitmessage
 ln -sf $PWD/irbrc                       ~/.irbrc
 ln -sf $PWD/tmux.conf                   ~/.tmux.conf
 ln -sf $PWD/oh-my-zsh                   ~/.oh-my-zsh
-ln -sf ~/.oh-my-zsh/zshrc ~/.zshrc
+ln -sf ~/.oh-my-zsh/zshrc               ~/.zshrc
 mkdir -p ~/.fonts
 ln -sf $PWD/fonts/ubuntu-mono-powerline ~/.fonts/ubuntu-mono-powerline
 
 echo 'Install some basic packages'
-sudo apt-get install g++ ncurses-term vim-nox vim-gui-common git ack-grep tig exuberant-ctags tmux zsh build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+sudo apt-get install g++ ncurses-term vim-nox vim-gui-common git ack-grep tig exuberant-ctags tmux zsh build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion ruby1.9.1
 
 
 echo "ack-grep is a lame name! Vim plugin won't recognize it"
@@ -28,7 +28,7 @@ echo 'Install rvm'
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # This loads RVM into a shell session.
 rvm install 1.9.3-head
-rvm use --default 1.9.3
+rvm use --default 1.9.3-head
 
 echo 'Change default shell to zsh'
 chsh -s /bin/zsh
