5d81d13277d50196a47bd8866308e575a5f4de3b
Leandro Facchinetti <leafac@gmail.com>
2012-10-01 01:55:34 -0300

    Packages are now executable \o/

---
+++ packages.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Add the following entry in /etc/pacman.conf
+# to be able to install yaourt
+# 
+# [archlinuxfr]
+# Server = http://repo.archlinux.fr/x86_64
+
+
+yaourt -S --noconfirm       \
+sudo                        \
+xorg-server                 \
+xorg-xinit                  \
+xorg-server-utils           \
+mesa                        \
+ttf-dejavu                  \
+ttf-droid                   \
+xfce4                       \
+xfce4-goodies               \
+slim                        \
+unzip                       \
+unrar                       \
+ntfs-3g                     \
+wicd                        \
+wicd-gtk                    \
+pmount                      \
+epdfview                    \
+parole                      \
+gstreamer0.10-plugins       \
+chromium                    \
+flashplugin                 \
+xcursor-neutral             \
+slim-themes                 \
+archlinux-themes-slim       \
+archlinux-wallpaper         \
+openssh                     \
+tmux                        \
+git                         \
+ruby                        \
+zsh                         \
+gvim                        \
+tig                         \
+ack                         \
+ctags                       \
+nodejs                      \
+postgresql                  \
+pgadmin3                    \
+eclipse                     \
+tree                        \
+gimp                        \
+dnsutils                    \
+calibre                     \
+ttf-ms-fonts                \
+package-query               \
+squeeze-git                 \
+dropbox                     \
+thunar-dropbox              \
+dropbox-daemon              \
+elementary-xfce-icons       \
+herrie                      \
+shutter
+
+
+
+# gem install                 \
+# bundler                     \
+# wirble                      \
+# pry
