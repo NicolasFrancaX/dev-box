f0c059c567c2fead59be82704355bbb2dbcfa3df
Leandro Facchinetti <leafac@gmail.com>
2013-03-08 17:31:27 -0300

    Pry everywhere

--- ruby/manifests/init.pp
+++ ruby/manifests/init.pp
@@ -1,5 +1,15 @@
 class ruby {
 
+  file {"$home/.irbrc":
+    ensure => link,
+    target => "$basedir/ruby/files/irbrc",
+  }
+
+  file {"$home/.pryrc":
+    ensure => link,
+    target => "$basedir/ruby/files/pryrc",
+  }
+
   file {"$home/.gemrc":
     ensure => link,
     target => "$basedir/ruby/files/gemrc",
