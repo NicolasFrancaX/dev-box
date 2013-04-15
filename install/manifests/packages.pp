754a3919f361c48f4eaf5edcf556338fea8485b5
Leandro Facchinetti <leafac@gmail.com>
2013-04-15 10:02:44 -0300

    Refactor packages list

--- install/manifests/packages.pp
+++ install/manifests/packages.pp
@@ -1,132 +1,71 @@
 class install::packages {
 
-  package {'gvim':}
-
-  package {'ttf-dejavu':}
-
-  package {'ttf-droid':}
-
-  package {'zip':}
-
-  package {'unzip':}
-
-  package {'unrar':}
-
-  package {'ntfs-3g':}
-
-  package {'wicd':}
-
-  package {'wicd-gtk':}
-
-  package {'pmount':}
-
-  package {'evince':}
-
-  package {'parole':}
-
-  package {'gstreamer0.10-plugins':}
-
-  package {'chromium':}
-
-  package {'flashplugin':}
-
-  package {'xcursor-neutral':}
-
-  package {'slim-themes':}
-
-  package {'archlinux-themes-slim':}
-
-  package {'archlinux-wallpaper':}
-
-  package {'xscreensaver':}
-
-  package {'openssh':}
-
-  package {'tmux':}
-
-  package {'htop':}
-
-  package {'irssi':}
-
-  package {'gnu-netcat':}
-
-  package {'git':}
-
-  package {'ruby':}
-
-  package {'ruby-docs':}
-
-  package {'zsh':}
-
-  package {'tig':}
-
-  package {'ack':}
-
-  package {'ctags':}
-
-  package {'nodejs':}
-
-  package {'postgresql':}
-
-  package {'pgadmin3':}
-
-  package {'eclipse':}
-
-  package {'tree':}
-
-  package {'gimp':}
-
-  package {'dnsutils':}
-
-  package {'calibre':}
-
-  package {'ttf-ms-fonts':}
-
-  package {'file-roller':}
-
-  package {'dropbox':}
-
-  package {'thunar-dropbox':}
-
-  package {'dropbox-daemon':}
-
-  package {'elementary-xfce-icons':}
-
-  package {'herrie':}
-
-  package {'shutter':}
-
-  package {'truecrypt':}
-
-  package {'xclip':}
-
-  package {'virtualbox':}
-
-  package {'virtualbox-guest-iso':}
-
-  package {'python':}
-
-  package {'python-pip':}
-
-  package {'espeak':}
-
-  package {'cloc':}
-
-  package {'figlet':}
-
-  package {'heroku-toolbelt':}
-
-  package {'whois':}
-
-  package {'graphviz':}
-
-  package {'gparted':}
-
-  package {'inkscape':}
-
-  package {'sshfs':}
-
-  package {'wget':}
+  package {[
+    'gvim',
+    'ttf-dejavu',
+    'ttf-droid',
+    'zip',
+    'unzip',
+    'unrar',
+    'ntfs-3g',
+    'wicd',
+    'wicd-gtk',
+    'pmount',
+    'evince',
+    'parole',
+    'gstreamer0.10-plugins',
+    'chromium',
+    'flashplugin',
+    'xcursor-neutral',
+    'slim-themes',
+    'archlinux-themes-slim',
+    'archlinux-wallpaper',
+    'xscreensaver',
+    'openssh',
+    'tmux',
+    'htop',
+    'irssi',
+    'gnu-netcat',
+    'git',
+    'ruby',
+    'ruby-docs',
+    'zsh',
+    'tig',
+    'ack',
+    'ctags',
+    'nodejs',
+    'postgresql',
+    'pgadmin3',
+    'eclipse',
+    'tree',
+    'gimp',
+    'dnsutils',
+    'calibre',
+    'ttf-ms-fonts',
+    'file-roller',
+    'dropbox',
+    'thunar-dropbox',
+    'dropbox-daemon',
+    'elementary-xfce-icons',
+    'herrie',
+    'shutter',
+    'truecrypt',
+    'xclip',
+    'virtualbox',
+    'virtualbox-guest-iso',
+    'python',
+    'python-pip',
+    'espeak',
+    'cloc',
+    'figlet',
+    'heroku-toolbelt',
+    'whois',
+    'graphviz',
+    'gparted',
+    'inkscape',
+    'sshfs',
+    'wget',
+  ]:}
 
   # http://drbunsen.github.com/formd/
 
