92be836d6674efb88695b17d696ae8c26c81f0c2
Leandro Facchinetti <git@leafac.com>
2013-09-17 08:25:31 -0300

    Rudimentary ppa support

--- modules/ubuntu/manifests/install_ppa_support.pp
+++ modules/ubuntu/manifests/install_ppa_support.pp
@@ -1,3 +1,11 @@
 class ubuntu::install_ppa_support {
-  package { 'python-software-properties': }
+  exec { 'update packages to install ppa support':
+    command => 'apt-get update',
+    user    => 'root',
+    group   => 'root',
+  }
+
+  package { ['python-software-properties', 'build-essential']:
+    require => Exec['update packages to install ppa support'],
+  }
 }
