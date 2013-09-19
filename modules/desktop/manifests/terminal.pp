ddcc092be3515b19b6a9b9b088c73d1e31397b5b
Leandro Facchinetti <git@leafac.com>
2013-09-18 21:06:19 -0300

    Install tomorrow for terminal as a preset

--- modules/desktop/manifests/terminal.pp
+++ modules/desktop/manifests/terminal.pp
@@ -9,7 +9,12 @@ class desktop::terminal {
 
   file { "$home/.config/xfce4/terminal/terminalrc":
     ensure  => link,
-    target  => "$modulepath/desktop/files/terminalrc",
+    target  => "$modulepath/desktop/files/terminal/terminalrc",
     require => File["$home/.config/xfce4/terminal"],
+  }
+
+  file { "/usr/share/xfce4/terminal/colorschemes/tomorrow.themes":
+    ensure  => link,
+    target  => "$modulepath/desktop/files/terminal/colorschemes/tomorrow.theme",
   }
 }
