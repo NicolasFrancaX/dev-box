a63a7ac66ddef274225d97ce4184384d880024b2
Leandro Facchinetti <git@leafac.com>
2013-10-14 12:32:29 -0300

    Fix chruby paths to remove

--- modules/ruby/manifests/chruby.pp
+++ modules/ruby/manifests/chruby.pp
@@ -3,7 +3,7 @@ class ruby::chruby(
   $ruby_install_version = '0.3.0',
 ) {
   exec { "install chruby $chruby_version":
-    command => "wget -O chruby-${chruby_version}.tar.gz https://github.com/postmodern/chruby/archive/v${chruby_version}.tar.gz && tar -xzvf chruby-${chruby_version}.tar.gz && cd chruby-${chruby_version} && make install && rm chruby-${chruby_version}.tar.gz",
+    command => "wget -O chruby-${chruby_version}.tar.gz https://github.com/postmodern/chruby/archive/v${chruby_version}.tar.gz && tar -xzvf chruby-${chruby_version}.tar.gz && (cd chruby-${chruby_version} && make install) && rm chruby-${chruby_version}.tar.gz && rm -rf chruby-${chruby_version}",
     creates => '/usr/local/share/chruby/chruby.sh',
     cwd     => '/tmp',
     user    => 'root',
@@ -11,7 +11,7 @@ class ruby::chruby(
   }
 
   exec { "install ruby-install $ruby_install_version":
-    command => "wget -O ruby-install-${ruby_install_version}.tar.gz https://github.com/postmodern/ruby-install/archive/v${ruby_install_version}.tar.gz && tar -xzvf ruby-install-${ruby_install_version}.tar.gz && cd ruby-install-${ruby_install_version}/ && make install && rm ruby-install-${ruby_install_version}.tar.gz",
+    command => "wget -O ruby-install-${ruby_install_version}.tar.gz https://github.com/postmodern/ruby-install/archive/v${ruby_install_version}.tar.gz && tar -xzvf ruby-install-${ruby_install_version}.tar.gz && (cd ruby-install-${ruby_install_version}/ && make install) && rm ruby-install-${ruby_install_version}.tar.gz && rm -rf ruby-install-${ruby_install_version}",
     creates => '/usr/local/share/ruby-install/ruby-install.sh',
     cwd     => '/tmp',
     user    => 'root',
